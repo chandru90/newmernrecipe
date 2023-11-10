e448a4497855914ce63a90daf958714d8e8d0246
chandru90 <iochandru90@gmail.com>
2023-11-10 11:38:37 +0530

    new commit

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
   app.use(
     cors({
-      origin: "https://newmernrecipe.vercel.app/",
+      origin: "https://newmernrecipe.vercel.app",
       methods: "POST,GET",
       credentials: true
     })
