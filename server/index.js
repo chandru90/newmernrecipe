42643c793fbeae859e3954bafd295242018c4b9f
chandru90 <iochandru90@gmail.com>
2023-11-10 14:27:44 +0530

    new commit

--- server/index.js
+++ server/index.js
@@ -16,8 +16,8 @@ app.use(express.json());
     })
   );
 
-app.use("/auth", userRouter);
-app.use("/recipes", recipesRouter);
+app.use("https://serve-1sxs.onrender.com/auth", userRouter);
+app.use("https://serve-1sxs.onrender.com/recipes", recipesRouter);
 
 mongoose.connect(
   "mongodb+srv://admin:admin@chancluster.qrpx1av.mongodb.net/recipe",
